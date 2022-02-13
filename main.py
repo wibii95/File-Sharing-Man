87fda5f5056013472c1fa40b92990dcb0a15ee28
mrismanaziz <mrismanaziz@gmail.com>
2022-02-13 21:15:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 import sys
 
 from bot import Bot
-from config import ADMINS, LOGGER, OWNER_ID
-from requests import get
+from config import ADMINS, LOGGER, OWNER_ID, blacklistman
 
 try:
     Bot().run()
-    blacklistman = get(
-        "https://raw.githubusercontent.com/mrismanaziz/Reforestation/master/manblacklist.json"
-    ).json()
     if OWNER_ID and ADMINS in blacklistman:
-        LOGGER.warning(
+        print(
             "MAKANYA GA USAH BERTINGKAH GOBLOK, BOTnya GUA MATIIN NAJIS BANGET DIPAKE JAMET KEK LU.\nCredits: @mrismanaziz"
         )
         sys.exit(1)
 except Exception as e:
-    LOGGER.info(str(e), exc_info=True)
+    print(str(e), exc_info=True)
     sys.exit(1)
